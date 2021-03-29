cbfe9517daa2479530cec3314f602a765895f82f
“Bugs <mevett@emich.edu>
2021-03-29 11:29:38 -0400

    initial commit
    Added a unity project

---
+++ ApplePicker2019.4/Assets/ApplePicker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplePicker : MonoBehaviour
+{
+
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -4f;
+    public float basketSpacingY = 2f;
+
+    void Start()
+    {
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+        }
+    }
+}
